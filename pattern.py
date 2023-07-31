93932a09e668be4e5a2f3ce7471ab566c9e75d29
meghana <meghanamahesh0407@gmail.com>
2023-07-31 09:28:34 +0530

    adding file

---
+++ pattern.py
@@ -0,0 +1,22 @@
+#pattern square
+#--------------
+
+# limit=int(input("Enter the row:"))
+# for i in range(0,limit):
+#     print("*"*4)
+
+#pattern triangle
+# ----------------
+# limit=int(input("Enter the row:"))
+# for i in range(1,limit):
+#    #for e in range(0,i+1):
+#      print("*"*i)
+
+
+
+
+
+limit=int(input("Enter the row:"))
+for i in range(1,limit):
+   #for e in range(0,i+1):
+     print("*"*i)
