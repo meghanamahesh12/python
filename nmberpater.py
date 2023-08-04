b05cf1a7620df0958cde647992c83e0f10f37890
VishnuIsHere <meghanamahesh0407@gmail.com>
2023-08-04 09:35:48 +0530

    adding file

---
+++ nmberpater.py
@@ -0,0 +1,43 @@
+# limit=int(input("Enter the raw:"))
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#       print(j,end='')
+#     print()
+
+
+
+    #nmbrpattern2
+
+
+# limit=int(input("Enter the raw:"))
+# for i in range(1,limit+1):
+#     for j in range(i):
+#       print(i,end='')
+#     print()
+
+
+
+
+    # nmbrpatter3
+
+
+# limit=int(input("Enter the raw:"))
+# for i in range(1,limit+1):
+#     for j in range(1,i):
+#       print(j%2,end='')
+#     print() 
+
+
+
+# # numberparrtern353
+
+limit=int(input("Enter the raw:"))
+for i in range(0,limit+1):
+    for j in range(0,i+1):
+      print(j%5,end='')
+    print() 
+
+
+
+
+
