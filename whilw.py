895a897aabd041c2c61f676d43eb4b297991569f
meghana <meghanamahesh0407@gmail.com>
2023-08-14 09:20:23 +0530

    file added

--- whilw.py
+++ whilw.py
@@ -1,28 +1,34 @@
-# a=int(input("enter a number :"))
-# i=0
+
 # sumo=0
 # sume=0
-# while i<=a:
-#    if a%2==0:
-#     sumo=sumo+a
-#     print("sum of odd numbers",sumo)
+# a=int(input("enter a number :"))
+# n=0
+# while n<=a:
+#    if n%2==0:
+#     sumo+=n 
 #    else: 
-#     sume=sume+a 
-#     print("sum of even numbers",sume)
-# i+=1    21
+#     sume+=n
+#    n+=1
+# a=int(input("enter a number :"))
+# print("sum of odd numbers",sumo)    
+
+
+#multipication
+
+
+    #even
 
 
 
 
-
-
+sume=0
 a=int(input("enter a number :"))
-sum = 0
-i = 0
-while i<=a:
-   if i%2==0:
-    print("sum of even numbers",sum)
-   else: 
-      print("sum of odd numbers",sum)   
-sum+=i
-i+=1
+n=0
+while n<=a:
+   if n%2==0:
+    sume+=n 
+   n+=1
+print("sum of odd numbers",sumo)    
+
+
+
