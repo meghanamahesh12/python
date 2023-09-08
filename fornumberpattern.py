85df4fed3f31ec9bb8559a4277a775c0a6d53c53
meghana <meghanamahesh0407@gmail.com>
2023-09-08 09:19:30 +0530

    new

---
+++ fornumberpattern.py
@@ -0,0 +1,41 @@
+#1 to 10
+
+# i=1
+# for i in range(1,11):
+#     print(i)
+
+#odd number
+
+# for i in range(1,11,2):
+#     print(i)
+
+
+
+
+# "*"
+# --------
+
+# for i in range(1,11):
+#     for j in range(1,i):
+#         print("*",end="")
+#     print("\n")    
+
+
+#1
+#12
+
+
+# for i in range(1,11):
+#     for j in range(1,i):
+#         print(j,end="")
+#     print("\n")
+
+
+#1
+#22
+
+# for i in range(0,10):
+#     for j in range(0,i):
+#         print(i,end="")
+#     print("\n")
+
